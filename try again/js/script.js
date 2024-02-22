56f2b37c01bc7818b3d4348e0ed1bfe7343b3432
Jakub Bembenek <kuba.bembenek@gmail.com>
2024-02-22 12:45:41 +0100

    slideshow!!

--- try again/js/script.js
+++ try again/js/script.js
@@ -35,3 +35,41 @@ marquee.addEventListener('mouseout', () => {
     marquee.start();
 });
 
+
+
+// splide thumbnail shi
+
+
+
+// main
+document.addEventListener( 'DOMContentLoaded', function () {
+    var main       = new Splide( '#image-carousel', {
+        cover :true,
+        type      : 'fade',
+        rewind    : true,
+        pagination: false,
+        arrows    : false,
+      heightRatio: 0.5,
+    } );
+
+
+  // thumbnail
+  var thumbnails = new Splide( '#thumbnail-carousel', {
+    fixedWidth  : 100,
+    fixedHeight : 60,
+    gap         : 10,
+    rewind      : true,
+    pagination  : false,
+    isNavigation: true,
+    breakpoints : {
+      600: {
+        fixedWidth : 60,
+        fixedHeight: 44,
+      },
+    },
+  } );
+
+  main.sync( thumbnails );
+  main.mount();
+  thumbnails.mount();
+} );
